6da46e9a8012f6318ac9e6f528471769ee3b79bf
flyboy12 <hakaneren@expe.ro>
2025-01-01 21:31:58 +0300

    Yarp api gateways made it.

---
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.RateLimiting;
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+// Add services to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseRateLimiter();
+
+app.MapReverseProxy();
+
+app.Run();
